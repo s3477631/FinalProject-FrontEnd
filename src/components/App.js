a2693642d50ed2334d19df02631e270f728deb45
reez <zreye24@gmail.com>
2020-01-20 12:14:29 +1000

    add select elements

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,20 @@ import React from "react"
 
 export default function App() {
     return (
-        <h1>Hello World!</h1>
+        <>
+            <h1>Break Schedule</h1>
+            <select>
+                <option value="1">24/01/20</option>
+                <option value="2">25/01/20</option>
+                <option value="3">26/01/20</option>
+                <option value="4">27/01/20</option>
+            </select>
+            <select>
+                <option value="1">Floater 1</option>
+                <option value="2">Floater 2</option>
+                <option value="3">Floater 3</option>
+            </select>
+        </>
     )
 }
 
