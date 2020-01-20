4e3b9d3baa57bea550ac7f7a7d1da213595250b3
reez <zreye24@gmail.com>
2020-01-20 12:27:15 +1000

    write break component html

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,16 @@ export default function App() {
                 <option value="2">Floater 2</option>
                 <option value="3">Floater 3</option>
             </select>
+            <div style={{borderStyle: 'solid'}}>
+                <h4 class="break-title">Zach's 1st 15</h4>
+                <p class="scheduled-time">10:00 - 10:15</p>
+                <input type="checkbox" class="started" />
+                <p class="time-elapsed">05:23</p>
+                <input type="checkbox" class="finished" />
+                <p>Started</p>
+                <p>Time Elapsed</p>
+                <p>Finished</p>
+            </div>
         </>
     )
 }
