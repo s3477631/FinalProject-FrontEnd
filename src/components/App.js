bcc7c494696c6d7e20feef78deec7ef637ebdce3
reez <zreye24@gmail.com>
2020-01-20 13:38:34 +1000

    write manager stats view

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from "react"
 import FloaterView from "./FloaterView"
 import LoginView from "./LoginView"
 import ManagerUploadView from "./ManagerUploadView"
+import ManagerStatsView from "./ManagerStatsView"
+
 export default function App() {
     return (
         <>
-            <ManagerUploadView />
+            <ManagerStatsView />
         </>
     )
 }
