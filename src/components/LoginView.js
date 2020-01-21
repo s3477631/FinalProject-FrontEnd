13f4fce842eefc89d717031e5dd71b9f1e29bd8c
reez <zreye24@gmail.com>
2020-01-21 16:51:30 +1000

    going home

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -1,22 +1,49 @@
-import React, { useState } from "react"
+import React, { useReducer } from "react"
 import { useGlobalState } from "./store"
 
 export default function LoginView() {
+    
+    const initialState = {
+        username: null,
+        password: null
+    }
 
-    // const { loginDetails, loginDispatch } = useReducer((state, action) => {
+    const { loginDetails, loginDispatch } = useReducer((state, action) => {
+        switch (action.type) {
+            case "setUsername": {
+                return {
+                    ...state,
+                    username: action.data
+                }
+            }
+            case "setPassword": {
+                return {
+                    ...state, 
+                    password: action.data
+                }
+            }
+        }
+    }, initialState)
 
-    // }, null)
     const { dispatch } = useGlobalState()
 
-    const onSubmit = (event, username, pass) => {
+    const onSubmit = (event) => {
         event.preventDefault()
+        const elements = event.target.elements
+        const username = elements[0]
+        const password = elements[1]
+
+        debugger
         // retrieve token
         const token = "ASDF"
         onLogin(token, username)
     }
 
-    const onChange = event => {
-        
+    const onChange = (event, action) => {
+        loginDispatch({
+            type: action,
+            data: event.value
+        })
     }
 
     const onLogin = (token, username) => {
@@ -33,16 +60,16 @@ export default function LoginView() {
     return (
         <>
         <h1>Break Scheduler</h1>
-        <form>
+        <form onSubmit={(event) => onSubmit(event)}>
             <div>
                 <label for="username">Username:</label>
-                <input name="username" onChange={event => onChange(event)} />
+                <input name="username" onChange={event => onChange(event, "setUsername")} />
             </div>
             <div>
                 <label for="password">Password:</label>
-                <input name="password" type="password" onChange={event => onChange(event)}/>
+                <input name="password" type="password" onChange={event => onChange(event, "setPassword")}/>
             </div>
-            <button onClick={(event) => onSubmit(event, "floater", "password")}>Submit</button>
+            <button>Submit</button>
         </form>
         </>
     )
