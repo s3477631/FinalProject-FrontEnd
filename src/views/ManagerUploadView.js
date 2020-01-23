e2ea0034c04f8e4f014622fa7727aca6665d0ad6
reez <zreye24@gmail.com>
2020-01-23 16:02:36 +1000

    render reminder of upload form when user uploads file

--- src/views/ManagerUploadView.js
+++ src/views/ManagerUploadView.js
@@ -1,19 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 import ManagerNav from "../components/ManagerNav"
+import axios from "axios"
 
 export default function ManagerUploadView() {
+
+    const [ file, setFile ] = useState(null)
+    const [ loading, setLoading ] = useState(null)
+
+    const onFileAdded = (event) => {
+
+        // validate .csv format
+        setLoading(true)
+
+        // get autofill data from server
+        const data = new FormData()
+        const newFile = event.target.files[0]
+        data.append('csvFile', newFile)
+        axios.post("https://boiling-inlet-28252.herokuapp.com/csvupload", data)
+        .then(response => {
+            // update state
+            setFile(response)
+            setLoading(false)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
     return (
         <>
             <ManagerNav />
             <form>
                 <div>
-                    <input type="file"/>
+                    <input type="file" onChange={event=>onFileAdded(event)}/>
                 </div>
-                <div>
-                    <label>Select date </label>
-                    <input type="date"/>
-                </div>
-                <button>Generate Break Schedule</button>
+                { loading && <p>Loading...</p> }
+                {
+                    file && <>
+                        <div>
+                            <label>Select date </label>
+                            <input type="date"/>
+                        </div>
+                        <div>
+                            <label>Select goal time </label>
+                            <input type="time"/>
+                        </div>
+                        <div>
+                            <label>Select number of floaters </label>
+                            <input type="number"/>
+                        </div>
+                        <button>Generate Break Schedule</button>
+                    </>
+                }
             </form>
         </>
     )
